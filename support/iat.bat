b3dfaed83ac8ca0276d46f571148b43e6e92b900
tvcutsem <tvcutsem@c0a7138a-1c4d-0410-81cb-3328cd1d7e1d>
2007-12-10 14:40:24 +0000

    Added Matthias' changes
    git-svn-id: https://ambienttalk.googlecode.com/svn/iat/trunk@863 c0a7138a-1c4d-0410-81cb-3328cd1d7e1d

--- support/iat.bat
+++ support/iat.bat
@@ -1,11 +1,11 @@
 @rem BAT script for running iat on Windows
 
-@set JARPATH="ambienttalk2.jar;lib\antlr.jar;lib\java-getopt-1.0.13.jar"
-@set AT_HOME=%PWD%
+@set AT_HOME=.
+@set AT_CLASSPATH=".;%CLASSPATH%;%AT_HOME%\ambienttalk2.jar;%AT_HOME%\lib\antlr.jar;%AT_HOME%\lib\java-getopt-1.0.13.jar"
 
 @rem invoke the IAT shell via the JVM and:
 @rem - pass the AT_HOME and AT_OBJECTPATH environment variables to the JVM environment via -D
 @rem - make sure to include all the jar files in the ./lib directory
 @rem - invoke the main IAT class
 @rem - pass any parameters to this script to IAT
-@java -DAT_HOME=%AT_HOME% -DAT_OBJECTPATH=%AT_OBJECTPATH% -classpath %CLASSPATH%;%JARPATH% edu.vub.at.IAT %1 %2 %3 %4 %5 %6 %7 %8 %9
+@java -DAT_HOME=%AT_HOME% -DAT_OBJECTPATH=%AT_OBJECTPATH% -classpath %AT_CLASSPATH% edu.vub.at.IAT %1 %2 %3 %4 %5 %6 %7 %8 %9
