cd8a4349ea74db6fd26435ad179c8c00984a3aea
tvcutsem <tvcutsem@c0a7138a-1c4d-0410-81cb-3328cd1d7e1d>
2010-06-15 16:18:46 +0000

    updated iat.bat to reflect changes in iat shell script
    git-svn-id: https://ambienttalk.googlecode.com/svn/iat/trunk@2007 c0a7138a-1c4d-0410-81cb-3328cd1d7e1d

--- support/iat.bat
+++ support/iat.bat
@@ -3,11 +3,11 @@
 @set AT_HOME=.
 @set AT_CLASSPATH=".;%CLASSPATH%;%AT_HOME%\ambienttalk2.jar;%AT_HOME%\lib\antlr.jar;%AT_HOME%\lib\java-getopt-1.0.13.jar;%AT_HOME%\lib\jakarta-regexp-1.5.jar;%AT_HOME%\lib\jline-0.9.94.jar"
 
-@set DEFAULT_OBJPATH="%AT_OBJECTPATH%;at=%AT_HOME%\atlib\at;applications=%AT_HOME%\atlib\applications;bridges=%AT_HOME%\atlib\bridges;frameworks=%AT_HOME%\atlib\frameworks;demo=%AT_HOME\atlib\demo;test=%AT_HOME%\atlib\test"
+@set DEFAULT_OBJPATH="at=%AT_HOME%\atlib\at;applications=%AT_HOME%\atlib\applications;bridges=%AT_HOME%\atlib\bridges;frameworks=%AT_HOME%\atlib\frameworks;demo=%AT_HOME\atlib\demo;test=%AT_HOME%\atlib\test"
 
 @rem invoke the IAT shell via the JVM and:
 @rem - pass the AT_HOME and AT_OBJECTPATH environment variables to the JVM environment via -D
 @rem - make sure to include all the jar files in the ./lib directory
 @rem - invoke the main IAT class
 @rem - pass any parameters to this script to IAT
-@java -DAT_HOME=%AT_HOME% -DAT_INIT=%AT_HOME%\atlib\at\init\init.at -DAT_OBJECTPATH=%DEFAULT_OBJPATH% -classpath %AT_CLASSPATH% edu.vub.at.IAT %1 %2 %3 %4 %5 %6 %7 %8 %9
+@java -DAT_HOME=%AT_HOME% -DAT_INIT=%AT_HOME%\atlib\at\init\init.at -DAT_OBJECTPATH=%AT_OBJECTPATH% -DAT_LIBPATH=%DEFAULT_OBJPATH% -classpath %AT_CLASSPATH% edu.vub.at.IAT %1 %2 %3 %4 %5 %6 %7 %8 %9
