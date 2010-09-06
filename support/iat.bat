d074e2e6fa4ae9f91c30dadbc650aad25caf9bc9
tvcutsem <tvcutsem@c0a7138a-1c4d-0410-81cb-3328cd1d7e1d>
2010-09-06 12:30:03 +0000

    Fixed bug in iat.bat and also enabled it to be invoked cleanly from another directory
    git-svn-id: https://ambienttalk.googlecode.com/svn/iat/trunk@2361 c0a7138a-1c4d-0410-81cb-3328cd1d7e1d

--- support/iat.bat
+++ support/iat.bat
@@ -1,13 +1,19 @@
 @rem BAT script for running iat on Windows
 
-@set AT_HOME=.
-@set AT_CLASSPATH=".;%CLASSPATH%;%AT_HOME%\ambienttalk2.jar;%AT_HOME%\lib\antlr.jar;%AT_HOME%\lib\java-getopt-1.0.13.jar;%AT_HOME%\lib\jakarta-regexp-1.5.jar;%AT_HOME%\lib\jline-0.9.94.jar"
+@rem cd /d %0\..
+@pushd %0\..
 
-@set DEFAULT_OBJPATH="at=%AT_HOME%\atlib\at;applications=%AT_HOME%\atlib\applications;bridges=%AT_HOME%\atlib\bridges;frameworks=%AT_HOME%\atlib\frameworks;demo=%AT_HOME\atlib\demo;test=%AT_HOME%\atlib\test"
+@set AT_HOME=%CD%
+
+@set AT_CLASSPATH="%AT_HOME%;%CLASSPATH%;%AT_HOME%\ambienttalk2.jar;%AT_HOME%\jlib\antlr.jar;%AT_HOME%\jlib\java-getopt-1.0.13.jar;%AT_HOME%\jlib\jakarta-regexp-1.5.jar;%AT_HOME%\jlib\jline-0.9.94.jar"
+
+@set DEFAULT_OBJPATH="at=%AT_HOME%\atlib\at;applications=%AT_HOME%\atlib\applications;bridges=%AT_HOME%\atlib\bridges;frameworks=%AT_HOME%\atlib\frameworks;demo=%AT_HOME%\atlib\demo;test=%AT_HOME%\atlib\test"
 
 @rem invoke the IAT shell via the JVM and:
 @rem - pass the AT_HOME and AT_OBJECTPATH environment variables to the JVM environment via -D
-@rem - make sure to include all the jar files in the ./lib directory
+@rem - make sure to include all the jar files in the ./jlib directory
 @rem - invoke the main IAT class
 @rem - pass any parameters to this script to IAT
+
 @java -DAT_HOME=%AT_HOME% -DAT_INIT=%AT_HOME%\atlib\at\init\init.at -DAT_OBJECTPATH=%AT_OBJECTPATH% -DAT_LIBPATH=%DEFAULT_OBJPATH% -classpath %AT_CLASSPATH% edu.vub.at.IAT %1 %2 %3 %4 %5 %6 %7 %8 %9
+@popd
